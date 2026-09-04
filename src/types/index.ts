6a83e80c67c194d2414da615098c3e491cabbf1e
hel1xasura-hell <ayuintech@gmail.com>
2026-09-04 12:22:54 +0530

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -83,6 +83,7 @@ export interface RegisterFormValues {
 }
 
 export interface ProfileSetupFormValues {
+  name: string;
   username: string;
   country: CountryCode;
   explanationLanguage: ExplanationLanguage;
