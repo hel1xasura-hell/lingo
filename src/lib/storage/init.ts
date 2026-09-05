2872142724a013aaaedec7791b0788654456ca7c
hel1xasura-hell <ayuintech@gmail.com>
2026-09-05 10:44:53 +0530

    Update init.ts

--- src/lib/storage/init.ts
+++ src/lib/storage/init.ts
@@ -3,6 +3,7 @@ import { getDatabase } from "./db";
 export async function initializeLocalStorage() {
   try {
     await getDatabase();
+    console.log("Lingo local storage initialized.");
   } catch (error) {
     console.error("Lingo local storage initialization failed:", error);
   }
