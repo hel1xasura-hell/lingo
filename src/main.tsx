09cf5178f50705a47c197913b478ad521d130b0f
hel1xasura-hell <ayuintech@gmail.com>
2026-09-05 10:45:45 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { initializeLocalStorage } from "./lib/storage/init";
+
+initializeLocalStorage();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
